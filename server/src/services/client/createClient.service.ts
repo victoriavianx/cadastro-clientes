0f68fa2aa6cd31b6b784cad8ccd51ea6195dd622
victoriavianx <victoriagarcia963@gmail.com>
2022-09-20 21:27:37 -0300

    feat: create client service

---
+++ server/src/services/client/createClient.service.ts
@@ -0,0 +1,14 @@
+import { AppDataSource } from "../../data-source";
+
+import { IClientRequest } from "../../interfaces/client.interface";
+
+const createClientService = ({
+  name,
+  lastname,
+  email,
+  password,
+  phone,
+  cellphone,
+}: IClientRequest) => {};
+
+export default createClientService;
