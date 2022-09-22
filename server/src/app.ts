34eeeed4153ec6fae129b37cdf278e31de912552
victoriavianx <victoriagarcia963@gmail.com>
2022-09-21 23:11:44 -0300

    update: app config

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,20 @@
 import cors from "cors";
+import "express-async-errors";
 import express from "express";
+
+import handleAppErrorMiddleware from "./middlewares/handleAppError.middleware";
+
+import clientRoutes from "./routes/client.routes";
+import loginRoute from "./routes/login.routes";
+import contactRoute from "./routes/contact.routes";
 
 const app = express();
 app.use(express.json());
+
+app.use("/clients", clientRoutes);
+app.use("/contacts", contactRoute);
+app.use("/login", loginRoute);
+
+app.use(handleAppErrorMiddleware);
 
 export default app;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,12 @@
 import app from "./app";
 import { AppDataSource } from "./data-source";
 
-const init = () => {
-  async () => {
-    await AppDataSource.initialize().catch((err) => {
-      console.error("Error during Data Source initialization", err);
-    });
-  };
+(async () => {
+  await AppDataSource.initialize().catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
 
   app.listen(3001, () => {
     console.log("App running at http://localhost:3001");
   });
-};
-
-init();
+})();
