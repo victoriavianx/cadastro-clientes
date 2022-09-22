307fc04d1a80595da9ee666a36748d850aa3cd75
victoriavianx <victoriagarcia963@gmail.com>
2022-09-22 15:10:33 -0300

    feat: list, update and delete contact routes

--- server/src/routes/contact.routes.ts
+++ server/src/routes/contact.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 
 import createContact from "../controllers/contact/createContact.controller";
+import deleteContact from "../controllers/contact/deleteContact.controller";
+import listContacts from "../controllers/contact/listContacts.controller";
+import updateContact from "../controllers/contact/updateContact.controller";
 import verifyToken from "../middlewares/verifyToken.middleware";
 
 const contactRoute = Router();
 
 contactRoute.post("", verifyToken, createContact);
+contactRoute.get("/:clientId", verifyToken, listContacts);
+contactRoute.patch("/:contactId", verifyToken, updateContact);
+contactRoute.delete("/:contactId", verifyToken, deleteContact);
 
 export default contactRoute;
