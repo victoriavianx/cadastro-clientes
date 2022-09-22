4c302e2cfede2ede4b6fd73606c8384ad3ef4d65
victoriavianx <victoriagarcia963@gmail.com>
2022-09-21 23:04:27 -0300

    feat: login interface and update client interface

--- server/src/interfaces/client.interface.ts
+++ server/src/interfaces/client.interface.ts
@@ -3,11 +3,25 @@ export interface IClientRequest {
   lastname: string;
   email: string;
   password: string;
-  phone: number;
-  cellphone: number;
+  phone: string;
+  cellphone: string;
 }
 
 export interface IClient extends IClientRequest {
   id: string;
   registry_date: string;
+}
+
+export interface IClientLogin {
+  email: string;
+  password: string;
+}
+
+export interface IClientUpdate {
+  name?: string;
+  lastname?: string;
+  email?: string;
+  password?: string;
+  phone?: string;
+  cellphone?: string;
 }
