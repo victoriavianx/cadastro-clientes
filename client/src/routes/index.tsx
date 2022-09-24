437938c62ffd404b5658cee0973d03997f68eaf3
victoriavianx <victoriagarcia963@gmail.com>
2022-09-23 22:45:05 -0300

    feat: catch token

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,21 +1,36 @@
+import { useEffect } from "react";
+import { useAuth } from "../providers/client/authProvider";
 import { Switch, Route } from "react-router-dom";
+
 import Home from "../pages/Home/home";
 import Outset from "../pages/Outset/outset";
 import SignIn from "../pages/SignIn/signin";
 import SignUp from "../pages/SignUp/signup";
 import Update from "../pages/Update/update";
 
+import { getToken } from "../utils";
+
 const Routes = () => {
+  const { authenticated, setAuthenticated } = useAuth();
+
+  useEffect(() => {
+    const token = JSON.parse(getToken);
+
+    if (token) {
+      return setAuthenticated(true);
+    }
+  }, [authenticated, setAuthenticated]);
+
   return (
     <Switch>
       <Route exact path={"/"}>
         <Outset />
       </Route>
-      <Route exact path={"/signin"}>
-        <SignIn />
-      </Route>
       <Route exact path={"/signup"}>
         <SignUp />
+      </Route>
+      <Route exact path={"/signin"}>
+        <SignIn />
       </Route>
       <Route exact path={"/dashboard"}>
         <Home />
