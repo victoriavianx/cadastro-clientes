56bc2268414264ed0e7d52017192184babf7b914
victoriavianx <victoriagarcia963@gmail.com>
2022-09-23 22:38:36 -0300

    feat: login and modal interfaces

--- client/src/interfaces/index.ts
+++ client/src/interfaces/index.ts
@@ -6,3 +6,18 @@ export interface IClient {
   phone: string;
   cellphone: string;
 }
+
+export interface IFullClient extends IClient {
+  id: string;
+  registered_date: string;
+}
+
+export interface ILogin {
+  email: string;
+  password: string;
+}
+
+export interface IModal {
+  isOpen: boolean;
+  onClose: () => void;
+}
