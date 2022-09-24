ad467b01c452b4d5b0973b24a34ffa9d16c41109
victoriavianx <victoriagarcia963@gmail.com>
2022-09-23 22:38:03 -0300

    feat: list contacts

--- client/src/components/Table/table.tsx
+++ client/src/components/Table/table.tsx
@@ -1,3 +1,7 @@
+import { api } from "../../services/data-source";
+import { useEffect } from "react";
+
+import { FiTrash2, FiEdit } from "react-icons/fi";
 import {
   Table,
   Thead,
@@ -6,9 +10,36 @@ import {
   Th,
   Td,
   TableContainer,
+  useDisclosure,
 } from "@chakra-ui/react";
+import { getToken } from "../../utils";
+import { toast } from "react-toastify";
+import ModalEdit from "../ModalEdit/modal";
 
-const ContactTable = () => {
+const ContactTable = ({ showContacts, contacts }: any) => {
+  useEffect(() => {
+    showContacts();
+  }, [showContacts]);
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const token = JSON.parse(getToken);
+
+  const handleDelete = (id: string) => {
+    api
+      .delete(`/contacts/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        toast.success("Contato deletado");
+
+        return showContacts();
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <TableContainer marginTop={"2rem"}>
       <Table variant="striped" colorScheme={"blackAlpha"} color={"black"}>
@@ -18,15 +49,40 @@ const ContactTable = () => {
             <Th color={"black"}>Email</Th>
             <Th color={"black"}>Telefone</Th>
             <Th color={"black"}>Celular</Th>
+            <Th color={"black"}>Editar</Th>
+            <Th color={"black"}>Deletar</Th>
           </Tr>
         </Thead>
         <Tbody>
-          <Tr>
-            <Td>inches</Td>
-            <Td>millimetres (mm)</Td>
-            <Td>25.4</Td>
-            <Td>25.4</Td>
-          </Tr>
+          {contacts.map((contact: any) => (
+            <Tr key={contact.id}>
+              <Td>
+                {contact.name} {contact.lastname}
+              </Td>
+              <Td>{contact.email}</Td>
+              <Td>{contact.phone}</Td>
+              <Td>{contact.cellphone}</Td>
+              <Td>
+                {isOpen && (
+                  <ModalEdit
+                    isOpen={isOpen}
+                    onClose={onClose}
+                    showContacts={showContacts}
+                    contact={contact}
+                    id={contact.id}
+                  />
+                )}
+                <button onClick={onOpen}>
+                  <FiEdit />
+                </button>
+              </Td>
+              <Td>
+                <button onClick={() => handleDelete(contact.id)}>
+                  <FiTrash2 />
+                </button>
+              </Td>
+            </Tr>
+          ))}
         </Tbody>
       </Table>
     </TableContainer>
