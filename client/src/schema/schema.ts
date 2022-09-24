08d2c3aa46d736ce09429251dc80e4426a72976a
victoriavianx <victoriagarcia963@gmail.com>
2022-09-23 22:45:29 -0300

    feat: login e contact schemas

--- client/src/schema/schema.ts
+++ client/src/schema/schema.ts
@@ -20,3 +20,31 @@ export const schema = yup.object().shape({
   phone: yup.string().required("Campo obrigatório"),
   cellphone: yup.string().required("Campo obrigatório"),
 });
+
+export const loginSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Este email não é válido")
+    .required("Campo obrigatório"),
+  password: yup
+    .string()
+    .min(8, "Mínimo de 8 dígitos")
+    .required("Campo obrigatório"),
+});
+
+export const contactSchema = yup.object().shape({
+  name: yup
+    .string()
+    .max(200, "Máximo de 200 caracteres")
+    .required("Campo obrigatório"),
+  lastname: yup
+    .string()
+    .max(200, "Máximo de 200 caracteres")
+    .required("Campo obrigatório"),
+  email: yup
+    .string()
+    .email("Este email não é válido")
+    .required("Campo obrigatório"),
+  phone: yup.string().required("Campo obrigatório"),
+  cellphone: yup.string().required("Campo obrigatório"),
+});
