1195132ae68e178d7b361c0d53f61dc718242eac
victoriavianx <victoriagarcia963@gmail.com>
2022-09-23 22:47:54 -0300

    feat: toastify

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,25 @@ import "./App.css";
 import Routes from "./routes";
 import { GlobalStyles } from "./styles/global";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
     <>
       <GlobalStyles />
       <Routes />
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </>
   );
 }
